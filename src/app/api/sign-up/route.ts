cb3504dc5094ef65738f4037c6cfc00dc5f8f6fb
ASIMNEUPANE <asimneupane11@gmail.com>
2024-06-23 21:56:26 +0545

    adh

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,34 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import mongoose from "mongoose";
+
+export async function POST(request: Request) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await request.json();
+    const existingUserVerifiedByUsername = await UserModel.findOne({
+      username,
+      isVerified: true,
+    });
+    if (existingUserVerifiedByUsername) {
+      return Response.json(
+        { success: false, message: "User is already taken" },
+        { status: 400 },
+      );
+    }
+  } catch (error) {
+    console.error("Error registering user", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Error registering user",
+      },
+      {
+        status: 500,
+      },
+    );
+  }
+}
